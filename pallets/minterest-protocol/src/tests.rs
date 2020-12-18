e7ebfa1b5547b7c313b5f0ae6d1c49bdcaf03f1a
Alexandr Liutsko <alexandr.liutsko@gmail.com>
2020-12-18 13:08:28 +0200

    Al/BE redeem_wrapped function and unit-test for protocol pallet. (#11)
    Impl. redeem_wrapped func, unit-tests for repay, borrow, redeem and redeem_wrapped functions.

--- pallets/minterest-protocol/src/tests.rs
+++ pallets/minterest-protocol/src/tests.rs
@@ -62,6 +62,10 @@ fn redeem_underlying_should_work() {
 			MinterestProtocol::redeem_underlying(Origin::signed(ALICE), CurrencyId::DOT, 100),
 			Error::<Test>::NotEnoughLiquidityAvailable
 		);
+		assert_noop!(
+			MinterestProtocol::redeem_underlying(Origin::signed(ALICE), CurrencyId::MDOT, 20),
+			Error::<Test>::NotValidUnderlyingAssetId
+		);
 
 		assert_ok!(MinterestProtocol::redeem_underlying(
 			Origin::signed(ALICE),
@@ -71,6 +75,69 @@ fn redeem_underlying_should_work() {
 		assert_eq!(TestPools::get_reserve_available_liquidity(CurrencyId::DOT), 30);
 		assert_eq!(TestMTokens::free_balance(CurrencyId::DOT, &ALICE), 70);
 		assert_eq!(TestMTokens::free_balance(CurrencyId::MDOT, &ALICE), 38);
+	});
+}
+
+#[test]
+fn redeem_should_work() {
+	new_test_ext().execute_with(|| {
+		assert_ok!(MinterestProtocol::deposit_underlying(
+			Origin::signed(ALICE),
+			CurrencyId::DOT,
+			60
+		));
+		assert_eq!(TestPools::get_reserve_available_liquidity(CurrencyId::DOT), 60);
+		assert_eq!(TestMTokens::free_balance(CurrencyId::DOT, &ALICE), 40);
+		assert_eq!(TestMTokens::free_balance(CurrencyId::MDOT, &ALICE), 75);
+
+		assert_ok!(MinterestProtocol::redeem(Origin::signed(ALICE), CurrencyId::DOT));
+
+		assert_ok!(MinterestProtocol::deposit_underlying(
+			Origin::signed(ALICE),
+			CurrencyId::DOT,
+			60
+		));
+		assert_noop!(
+			MinterestProtocol::redeem_underlying(Origin::signed(BOB), CurrencyId::DOT, 30),
+			Error::<Test>::NotEnoughWrappedTokens
+		);
+
+		assert_noop!(
+			MinterestProtocol::redeem_underlying(Origin::signed(ALICE), CurrencyId::MDOT, 20),
+			Error::<Test>::NotValidUnderlyingAssetId
+		);
+	});
+}
+
+#[test]
+fn redeem_wrapped_should_work() {
+	new_test_ext().execute_with(|| {
+		assert_ok!(MinterestProtocol::deposit_underlying(
+			Origin::signed(ALICE),
+			CurrencyId::DOT,
+			60
+		));
+		assert_eq!(TestPools::get_reserve_available_liquidity(CurrencyId::DOT), 60);
+		assert_eq!(TestMTokens::free_balance(CurrencyId::DOT, &ALICE), 40);
+		assert_eq!(TestMTokens::free_balance(CurrencyId::MDOT, &ALICE), 75);
+
+		assert_ok!(MinterestProtocol::redeem_wrapped(
+			Origin::signed(ALICE),
+			CurrencyId::MDOT,
+			35
+		));
+		assert_eq!(TestPools::get_reserve_available_liquidity(CurrencyId::DOT), 32);
+		assert_eq!(TestMTokens::free_balance(CurrencyId::DOT, &ALICE), 68);
+		assert_eq!(TestMTokens::free_balance(CurrencyId::MDOT, &ALICE), 40);
+
+		assert_noop!(
+			MinterestProtocol::redeem_wrapped(Origin::signed(ALICE), CurrencyId::MDOT, 60),
+			Error::<Test>::NotEnoughWrappedTokens
+		);
+		assert_noop!(
+			MinterestProtocol::redeem_wrapped(Origin::signed(ALICE), CurrencyId::DOT, 20),
+			Error::<Test>::NotValidWrappedTokenId
+		);
 	});
 }
 
@@ -99,5 +166,66 @@ fn getting_assets_from_reserve_by_different_users_should_work() {
 		assert_eq!(TestPools::get_reserve_available_liquidity(CurrencyId::DOT), 67);
 		assert_eq!(TestMTokens::free_balance(CurrencyId::DOT, &BOB), 93);
 		assert_eq!(TestMTokens::free_balance(CurrencyId::MDOT, &BOB), 8);
+	});
+}
+
+#[test]
+fn borrow_should_work() {
+	new_test_ext().execute_with(|| {
+		assert_ok!(MinterestProtocol::deposit_underlying(
+			Origin::signed(ALICE),
+			CurrencyId::DOT,
+			60
+		));
+		assert_eq!(TestPools::get_reserve_available_liquidity(CurrencyId::DOT), 60);
+		assert_eq!(TestMTokens::free_balance(CurrencyId::DOT, &ALICE), 40);
+		assert_eq!(TestMTokens::free_balance(CurrencyId::MDOT, &ALICE), 75);
+
+		assert_noop!(
+			MinterestProtocol::borrow(Origin::signed(ALICE), CurrencyId::DOT, 100),
+			Error::<Test>::NotEnoughLiquidityAvailable
+		);
+		assert_noop!(
+			MinterestProtocol::borrow(Origin::signed(ALICE), CurrencyId::MDOT, 60),
+			Error::<Test>::NotValidUnderlyingAssetId
+		);
+
+		assert_ok!(MinterestProtocol::borrow(Origin::signed(ALICE), CurrencyId::DOT, 30));
+		assert_eq!(TestPools::get_reserve_available_liquidity(CurrencyId::DOT), 30);
+		assert_eq!(TestMTokens::free_balance(CurrencyId::DOT, &ALICE), 70);
+		assert_eq!(TestMTokens::free_balance(CurrencyId::MDOT, &ALICE), 75);
+	});
+}
+
+#[test]
+fn repay_should_work() {
+	new_test_ext().execute_with(|| {
+		assert_ok!(MinterestProtocol::deposit_underlying(
+			Origin::signed(ALICE),
+			CurrencyId::DOT,
+			60
+		));
+		assert_eq!(TestPools::get_reserve_available_liquidity(CurrencyId::DOT), 60);
+		assert_eq!(TestMTokens::free_balance(CurrencyId::DOT, &ALICE), 40);
+		assert_eq!(TestMTokens::free_balance(CurrencyId::MDOT, &ALICE), 75);
+
+		assert_ok!(MinterestProtocol::borrow(Origin::signed(ALICE), CurrencyId::DOT, 30));
+		assert_eq!(TestPools::get_reserve_available_liquidity(CurrencyId::DOT), 30);
+		assert_eq!(TestMTokens::free_balance(CurrencyId::DOT, &ALICE), 70);
+		assert_eq!(TestMTokens::free_balance(CurrencyId::MDOT, &ALICE), 75);
+
+		assert_noop!(
+			MinterestProtocol::repay(Origin::signed(ALICE), CurrencyId::MDOT, 10),
+			Error::<Test>::NotValidUnderlyingAssetId
+		);
+		assert_noop!(
+			MinterestProtocol::repay(Origin::signed(ALICE), CurrencyId::DOT, 100),
+			Error::<Test>::NotEnoughUnderlyingsAssets
+		);
+
+		assert_ok!(MinterestProtocol::repay(Origin::signed(ALICE), CurrencyId::DOT, 20));
+		assert_eq!(TestPools::get_reserve_available_liquidity(CurrencyId::DOT), 50);
+		assert_eq!(TestMTokens::free_balance(CurrencyId::DOT, &ALICE), 50);
+		assert_eq!(TestMTokens::free_balance(CurrencyId::MDOT, &ALICE), 75);
 	});
 }
