5396f500829644a8153fdb12ec7af495ba2c85fb
Alexandr Liutsko <alexandr.liutsko@gmail.com>
2021-01-04 13:37:40 +0200

    AL/BE-86. Accounts pallet. (#20)
    * [BE-86] Added accounts pallet to runtime.  Added add_member and remove_member functions to accounts pallet

---
+++ pallets/accounts/src/mock.rs
@@ -0,0 +1,87 @@
+/// Mocks for the accounts pallet.
+use frame_support::{impl_outer_event, impl_outer_origin, parameter_types};
+use sp_core::H256;
+use sp_runtime::{testing::Header, traits::IdentityLookup, Perbill};
+
+use super::*;
+use sp_io::TestExternalities;
+
+impl_outer_origin! {
+	pub enum Origin for Test {}
+}
+
+mod accounts {
+	pub use crate::Event;
+}
+
+impl_outer_event! {
+	pub enum TestEvent for Test {
+		frame_system<T>,
+		accounts<T>,
+	}
+}
+
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub struct Test;
+parameter_types! {
+	pub const BlockHashCount: u64 = 250;
+	pub const MaximumBlockWeight: u32 = 1024;
+	pub const MaximumBlockLength: u32 = 2 * 1024;
+	pub const AvailableBlockRatio: Perbill = Perbill::one();
+}
+
+type AccountId = u32;
+
+impl frame_system::Trait for Test {
+	type Origin = Origin;
+	type Call = ();
+	type Index = u64;
+	type BlockNumber = u64;
+	type Hash = H256;
+	type Hashing = ::sp_runtime::traits::BlakeTwo256;
+	type AccountId = AccountId;
+	type Lookup = IdentityLookup<Self::AccountId>;
+	type Header = Header;
+	type Event = TestEvent;
+	type BlockHashCount = BlockHashCount;
+	type MaximumExtrinsicWeight = MaximumBlockWeight;
+	type MaximumBlockWeight = MaximumBlockWeight;
+	type DbWeight = ();
+	type BlockExecutionWeight = ();
+	type ExtrinsicBaseWeight = ();
+	type MaximumBlockLength = MaximumBlockLength;
+	type AvailableBlockRatio = AvailableBlockRatio;
+	type Version = ();
+	type PalletInfo = ();
+	type OnNewAccount = ();
+	type OnKilledAccount = ();
+	type AccountData = ();
+	type BaseCallFilter = ();
+	type SystemWeightInfo = ();
+}
+
+impl Trait for Test {
+	type Event = TestEvent;
+}
+
+pub const ALICE: AccountId = 1;
+pub const BOB: AccountId = 2;
+pub type TestAccounts = Module<Test>;
+pub type System = frame_system::Module<Test>;
+
+// pub(crate) fn new_test_ext() -> sp_io::TestExternalities {
+// 	let t = frame_system::GenesisConfig::default().build_storage::<Test>().unwrap();
+//
+// 	t.into()
+// }
+
+pub struct ExternalityBuilder;
+
+impl ExternalityBuilder {
+	pub fn build() -> TestExternalities {
+		let storage = system::GenesisConfig::default().build_storage::<Test>().unwrap();
+		let mut ext = TestExternalities::from(storage);
+		ext.execute_with(|| System::set_block_number(1));
+		ext
+	}
+}
---
+++ pallets/accounts/src/tests.rs
@@ -0,0 +1,84 @@
+//! Tests for the accounts pallet.
+
+use super::*;
+use mock::*;
+
+use frame_support::{assert_noop, assert_ok, error::BadOrigin};
+
+#[test]
+fn add_member_should_work() {
+	ExternalityBuilder::build().execute_with(|| {
+		assert_noop!(TestAccounts::add_member(Origin::signed(ALICE), BOB), BadOrigin);
+
+		assert_ok!(TestAccounts::add_member(Origin::root(), ALICE));
+		let expected_event = TestEvent::accounts(RawEvent::AccountAdded(ALICE));
+		assert_eq!(System::events()[0].event, expected_event,);
+		assert!(<AllowedAccounts<Test>>::contains_key(ALICE));
+
+		assert_ok!(TestAccounts::add_member(Origin::root(), BOB));
+		assert_noop!(
+			TestAccounts::add_member(Origin::root(), ALICE),
+			Error::<Test>::AlreadyMember
+		);
+	});
+}
+
+#[test]
+fn cant_exceed_max_members() {
+	ExternalityBuilder::build().execute_with(|| {
+		// Add 16 members, reaching the max
+		for i in 0..16 {
+			assert_ok!(TestAccounts::add_member(Origin::root(), i));
+		}
+
+		// Try to add the 17th member exceeding the max
+		assert_noop!(
+			TestAccounts::add_member(Origin::root(), 16),
+			Error::<Test>::MembershipLimitReached
+		);
+	})
+}
+
+#[test]
+fn remove_member_should_work() {
+	ExternalityBuilder::build().execute_with(|| {
+		assert_ok!(TestAccounts::add_member(Origin::root(), ALICE));
+		assert_ok!(TestAccounts::add_member(Origin::root(), BOB));
+
+		assert_ok!(TestAccounts::remove_member(Origin::root(), BOB));
+		assert_noop!(
+			TestAccounts::remove_member(Origin::root(), ALICE),
+			Error::<Test>::MustBeAtLeastOneMember
+		);
+
+		// Test that the expected events were emitted
+		let our_events = System::events()
+			.into_iter()
+			.map(|r| r.event)
+			.filter_map(|e| {
+				if let TestEvent::accounts(inner) = e {
+					Some(inner)
+				} else {
+					None
+				}
+			})
+			.collect::<Vec<_>>();
+
+		let expected_events = vec![
+			RawEvent::AccountAdded(1),
+			RawEvent::AccountAdded(2),
+			RawEvent::AccountRemoved(2),
+		];
+
+		assert_eq!(our_events, expected_events);
+
+		// check storage changes
+		assert!(<AllowedAccounts<Test>>::contains_key(ALICE));
+		assert!(!<AllowedAccounts<Test>>::contains_key(BOB));
+
+		assert_noop!(
+			TestAccounts::remove_member(Origin::root(), BOB),
+			Error::<Test>::NotMember
+		);
+	})
+}
