a247886dc6d01ee945a6320467035e1d18861be8
Alexandr Liutsko <alexandr.liutsko@gmail.com>
2021-02-08 13:19:48 +0200

    AL/BE-182. Create liquidation pools. (#43)
    * [BE-182] adding liquidation-pools pallet
    
    * [BE-182] Added pallet liquidation-pools to runtime
    
    * [BE-182] Added mock and tests files to liquidation-pools pallet
    
    * [BE-182] added function get_pool_available_liquidity to liquidation-pools pallet
    
    * [BE-182] added trait PoolsManager
    
    * [BE-182] Fixed all tests after adding new trait PoolManager
    
    * [BE-182] removed coverage yml
    
    * [BE-182] fixed cargo files
    
    * [BE-182] fixed ci yml file
    
    * [BE-182] fixed name generic type in runtime lib.rs

---
+++ pallets/liquidation-pools/src/mock.rs
@@ -0,0 +1,113 @@
+/// Mocks for the liquidation-pools pallet.
+use frame_support::{impl_outer_event, impl_outer_origin, parameter_types};
+pub use minterest_primitives::CurrencyId;
+use orml_currencies::Currency;
+use sp_core::H256;
+use sp_io::TestExternalities;
+use sp_runtime::{testing::Header, traits::IdentityLookup, Perbill};
+
+use super::*;
+
+mod liquidation_pools {
+	pub use crate::Event;
+}
+
+impl_outer_origin! {
+	pub enum Origin for Test {}
+}
+
+impl_outer_event! {
+	pub enum TestEvent for Test {
+		frame_system<T>,
+		orml_currencies<T>,
+		orml_tokens<T>,
+		liquidation_pools,
+	}
+}
+
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub struct Test;
+parameter_types! {
+	pub const BlockHashCount: u64 = 250;
+	pub const MaximumBlockWeight: u32 = 1024;
+	pub const MaximumBlockLength: u32 = 2 * 1024;
+	pub const AvailableBlockRatio: Perbill = Perbill::one();
+}
+
+pub type AccountId = u32;
+
+impl frame_system::Trait for Test {
+	type Origin = Origin;
+	type Call = ();
+	type Index = u64;
+	type BlockNumber = u64;
+	type Hash = H256;
+	type Hashing = ::sp_runtime::traits::BlakeTwo256;
+	type AccountId = AccountId;
+	type Lookup = IdentityLookup<Self::AccountId>;
+	type Header = Header;
+	type Event = TestEvent;
+	type BlockHashCount = BlockHashCount;
+	type MaximumExtrinsicWeight = MaximumBlockWeight;
+	type MaximumBlockWeight = MaximumBlockWeight;
+	type DbWeight = ();
+	type BlockExecutionWeight = ();
+	type ExtrinsicBaseWeight = ();
+	type MaximumBlockLength = MaximumBlockLength;
+	type AvailableBlockRatio = AvailableBlockRatio;
+	type Version = ();
+	type PalletInfo = ();
+	type OnNewAccount = ();
+	type OnKilledAccount = ();
+	type AccountData = ();
+	type BaseCallFilter = ();
+	type SystemWeightInfo = ();
+}
+
+impl orml_tokens::Trait for Test {
+	type Event = TestEvent;
+	type Balance = Balance;
+	type Amount = Amount;
+	type CurrencyId = CurrencyId;
+	type OnReceived = ();
+	type WeightInfo = ();
+}
+
+parameter_types! {
+	pub const GetNativeCurrencyId: CurrencyId = CurrencyId::MINT;
+}
+
+type NativeCurrency = Currency<Test, GetNativeCurrencyId>;
+
+impl orml_currencies::Trait for Test {
+	type Event = TestEvent;
+	type MultiCurrency = orml_tokens::Module<Test>;
+	type NativeCurrency = NativeCurrency;
+	type GetNativeCurrencyId = GetNativeCurrencyId;
+	type WeightInfo = ();
+}
+
+parameter_types! {
+	pub const LiquidationPoolsModuleId: ModuleId = ModuleId(*b"min/lqdn");
+}
+
+impl Trait for Test {
+	type Event = TestEvent;
+	type ModuleId = LiquidationPoolsModuleId;
+	type MultiCurrency = orml_tokens::Module<Test>;
+}
+
+type Amount = i128;
+pub type _TestLiquidationPools = Module<Test>;
+pub type System = frame_system::Module<Test>;
+
+pub struct ExternalityBuilder;
+
+impl ExternalityBuilder {
+	pub fn build() -> TestExternalities {
+		let storage = frame_system::GenesisConfig::default().build_storage::<Test>().unwrap();
+		let mut ext = TestExternalities::from(storage);
+		ext.execute_with(|| System::set_block_number(1));
+		ext
+	}
+}
