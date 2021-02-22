40a3f347f9d8bfa47d225dcfd0bb13e45f2166c4
Eugene <57300277+Apollo50@users.noreply.github.com>
2021-02-22 13:03:00 +0200

    Ep/be 91 add setter for collateral param (#54)
    * [BE-91] Add setter for Collateral factor to controller pallet.
    
    * [BE-91] Add test for fn set_collateral_factor.
    
    * [BE-91] Change param liquidation_fee -> liquidation_incentive. Add liquidation incentive param size checking.
    
    * [BE-91] Fix test for fn set_liquidation_incentive.
    
    * [BE-91] Change risk-manager pallet version.

--- service/src/chain_spec.rs
+++ service/src/chain_spec.rs
@@ -406,7 +406,7 @@ fn testnet_genesis(
 						max_attempts: 2,
 						min_sum: 200_000 * DOLLARS,                               // In USD. FIXME: temporary value.
 						threshold: FixedU128::saturating_from_rational(103, 100), // 3%
-						liquidation_fee: FixedU128::saturating_from_rational(103, 100), // 3%
+						liquidation_incentive: FixedU128::saturating_from_rational(103, 100), // 3%
 					},
 				),
 				(
@@ -415,7 +415,7 @@ fn testnet_genesis(
 						max_attempts: 2,
 						min_sum: 100_000 * DOLLARS,                               // In USD. FIXME: temporary value.
 						threshold: FixedU128::saturating_from_rational(103, 100), // 3%
-						liquidation_fee: FixedU128::saturating_from_rational(103, 100), // 3%
+						liquidation_incentive: FixedU128::saturating_from_rational(103, 100), // 3%
 					},
 				),
 				(
@@ -424,7 +424,7 @@ fn testnet_genesis(
 						max_attempts: 2,
 						min_sum: 200_000 * DOLLARS,                               // In USD. FIXME: temporary value.
 						threshold: FixedU128::saturating_from_rational(103, 100), // 3%
-						liquidation_fee: FixedU128::saturating_from_rational(103, 100), // 3%
+						liquidation_incentive: FixedU128::saturating_from_rational(103, 100), // 3%
 					},
 				),
 				(
@@ -433,7 +433,7 @@ fn testnet_genesis(
 						max_attempts: 2,
 						min_sum: 200_000 * DOLLARS,                               // In USD. FIXME: temporary value.
 						threshold: FixedU128::saturating_from_rational(103, 100), // 3%
-						liquidation_fee: FixedU128::saturating_from_rational(103, 100), // 3%
+						liquidation_incentive: FixedU128::saturating_from_rational(103, 100), // 3%
 					},
 				),
 			],
