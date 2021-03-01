f768f83b0fb02c1a6c9b6af71e1219d62a4edc2b
Eugene <evgeni.valentino@gmail.com>
2021-03-01 19:09:26 +0200

    [MIN-72] Clippy fix

--- pallets/accounts/rpc/runtime-api/src/lib.rs
+++ pallets/accounts/rpc/runtime-api/src/lib.rs
@@ -7,7 +7,6 @@
 #![allow(clippy::unnecessary_mut_passed)]
 
 use codec::Codec;
-use sp_std::prelude::*;
 
 // Here we declare the runtime API. It is implemented it the `impl` block in
 // runtime amalgamator file (the `runtime/src/lib.rs`)
