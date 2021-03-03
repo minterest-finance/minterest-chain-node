fc4994bc4908290fc561a5ba1ebb208db2cd17a9
Vadim <vadim.p@blaize.tech>
2021-03-03 16:36:32 +0200

    [BE-220] Fix RPC sponse struct serialization error

--- pallets/controller/rpc/runtime-api/src/lib.rs
+++ pallets/controller/rpc/runtime-api/src/lib.rs
@@ -12,7 +12,7 @@ use sp_core::RuntimeDebug;
 use sp_std::prelude::*;
 
 #[cfg(feature = "std")]
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
 
 #[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
 #[derive(Encode, Decode, Eq, PartialEq, Default, RuntimeDebug)]
@@ -25,8 +25,24 @@ pub struct PoolState {
 #[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
 #[derive(Encode, Decode, Eq, PartialEq, Default, RuntimeDebug)]
 pub struct UserPoolBalanceData {
+	#[cfg_attr(feature = "std", serde(serialize_with = "serialize_as_string"))]
+	#[cfg_attr(feature = "std", serde(deserialize_with = "deserialize_from_string"))]
 	pub total_supply: Balance,
+	#[cfg_attr(feature = "std", serde(serialize_with = "serialize_as_string"))]
+	#[cfg_attr(feature = "std", serde(deserialize_with = "deserialize_from_string"))]
 	pub total_borrowed: Balance,
+}
+
+#[cfg(feature = "std")]
+fn serialize_as_string<S: Serializer, T: std::fmt::Display>(t: &T, serializer: S) -> Result<S::Ok, S::Error> {
+	serializer.serialize_str(&t.to_string())
+}
+
+#[cfg(feature = "std")]
+fn deserialize_from_string<'de, D: Deserializer<'de>, T: std::str::FromStr>(deserializer: D) -> Result<T, D::Error> {
+	let s = String::deserialize(deserializer)?;
+	s.parse::<T>()
+		.map_err(|_| serde::de::Error::custom("Parse from string failed"))
 }
 
 // Here we declare the runtime API. It is implemented it the `impl` block in
