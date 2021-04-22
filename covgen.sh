7fa074d1a4daf4b81ea0698834651fda31447e47
Anatolii Kurotych <anatolii.k@blaize.tech>
2021-04-22 14:39:41 +0300

    [MIN-9] Add script to generate coverage summary

--- covgen.sh
+++ covgen.sh
@@ -3,8 +3,7 @@
 # https://doc.rust-lang.org/nightly/unstable-book/compiler-flags/source-based-code-coverage.html
 
 # Requirements:
-# cargo install rustfilt
-# cargo install cargo-binutils
+# cargo install cargo-binutils rustfilt
 # sudo apt install jq
 
 # Usage: ./covgen.sh <pallet_name>
@@ -18,17 +17,23 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
+# Go to pallet, compile tests and get binary path
 cd pallets/$1
 bin_path=$(RUSTFLAGS="-Zinstrument-coverage" cargo test --no-run --message-format=json \
     | jq -r "select(.profile.test == true) | .filenames[]")
 cd ../..
 
+# Run binary to generate profraw file
 LLVM_PROFILE_FILE="formatjson5.profraw" $bin_path
-cargo profdata -- merge -sparse  formatjson5.profraw -o json5format.profdata
 
+# Generate html coverage report
+cargo profdata -- merge -sparse  formatjson5.profraw -o json5format.profdata
 cargo cov -- show -Xdemangler=rustfilt $bin_path \
     --ignore-filename-regex='/.cargo/registry|toolchains/nightly|mock.rs|tests.rs' \
     --instr-profile=json5format.profdata -format=html -output-dir=./covgen/$1/
 
+
 rm formatjson5.profraw
 rm json5format.profdata
+# This artifact occasionaly apeears.
+rm default.profraw 2> /dev/null || true
---
+++ covsum.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+# https://doc.rust-lang.org/nightly/unstable-book/compiler-flags/source-based-code-coverage.html
+
+# Requirements:
+# cargo install rustfilt cargo-binutils
+# sudo apt install jq
+
+# Usage: ./covsum.sh or ./covsum.sh > result.json
+
+# How it works. It runs unit tests for each pallet and takes coverage only from lib.rs.
+
+# See information about lines and regions coverage
+# https://doc.rust-lang.org/nightly/unstable-book/compiler-flags/source-based-code-coverage.html#interpreting-reports
+
+# Add pallet-name here to include in coverage report
+Pallets=('risk-manager' 'controller' 'dex' \
+    'liquidation-pools' 'liquidity-pools' \
+    'minterest-model' 'mnt-token' 'prices')
+
+set -e
+
+summary='[]'
+
+for pallet_name in "${Pallets[@]}"; do
+    # Go to pallet, compile tests and get binary path
+    cd pallets/$pallet_name
+    bin_path=$(RUSTFLAGS="-Zinstrument-coverage" cargo test --no-run --message-format=json \
+        | jq -r "select(.profile.test == true) | .filenames[]")
+    cd ../..
+
+    # Run binary to generate profraw file
+    LLVM_PROFILE_FILE="formatjson5.profraw" $bin_path &> /dev/null
+
+    # Generate summary
+    cargo profdata -- merge -sparse  formatjson5.profraw -o json5format.profdata
+    cov_sum=$(cargo cov -- export -Xdemangler=rustfilt --format='text' \
+        --ignore-filename-regex='/.cargo/registry|toolchains/nightly|mock.rs|tests.rs' \
+        --instr-profile=json5format.profdata  -summary-only  --object $bin_path)
+    cov_result=$(echo $cov_sum | jq ".data[].files[] | select(.filename==\"pallets/$pallet_name/src/lib.rs\").summary |
+        {pallet: \"$pallet_name\", lines_coverage: .lines.percent, regions_coverage: .regions.percent}")
+
+    summary=$(echo $summary | jq ".[length] |= . + $cov_result")
+done
+
+echo $summary | jq
+
+rm formatjson5.profraw
+rm json5format.profdata
+# This artifact occasionaly apeears.
+rm default.profraw 2> /dev/null || true
