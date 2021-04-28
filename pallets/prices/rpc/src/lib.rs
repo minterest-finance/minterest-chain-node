0bc7c085dd794042d39aa62fcaa82397f7aab4c5
Ales <ales.ananich@gmail.com>
2021-04-28 11:54:16 +0300

    fmt hotfix

--- pallets/prices/rpc/src/lib.rs
+++ pallets/prices/rpc/src/lib.rs
@@ -25,7 +25,6 @@ pub trait PricesApi<BlockHash> {
 	///  - `at` : Needed for runtime API use. Runtime API must always be called at a specific block.
 	/// Example:
 	///
-	///
 	#[rpc(name = "prices_getCurrentPrice")]
 	fn get_current_price(&self, at: Option<BlockHash>, currency_id: CurrencyId) -> Result<Option<Price>>;
 
