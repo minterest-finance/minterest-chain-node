81bca659c45563bfa2b3d589b64e9d481cf25b3b
Alexandr Liutsko <alexandr.liutsko@gmail.com>
2021-07-08 15:38:03 +0300

    [MIN-475] fix minterest-protocol tests

--- pallets/minterest-protocol/src/tests.rs
+++ pallets/minterest-protocol/src/tests.rs
@@ -221,10 +221,7 @@ fn deposit_underlying_should_work() {
 			assert!(System::events().iter().any(|record| record.event == expected_event));
 
 			// Check liquidation_attempts has been reset.
-			assert_eq!(
-				TestPools::get_pool_user_data(DOT, &ALICE).liquidation_attempts,
-				u8::zero()
-			);
+			assert_eq!(TestPools::get_user_data(DOT, &ALICE).liquidation_attempts, u8::zero());
 
 			// MDOT pool does not exist.
 			assert_noop!(
