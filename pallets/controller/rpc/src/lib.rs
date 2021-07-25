534609156252607697c268154233d43817bb8bdd
jekshmek <yaroshjeka@gmail.com>
2021-07-25 17:44:40 +0300

    Update pallets/controller/rpc/src/lib.rs
    Co-authored-by: Eugene <57300277+Apollo50@users.noreply.github.com>

--- pallets/controller/rpc/src/lib.rs
+++ pallets/controller/rpc/src/lib.rs
@@ -49,7 +49,7 @@ pub trait ControllerRpcApi<BlockHash, AccountId> {
 	/// - [`exchange_rate`](`PoolState::exchange_rate`): the Exchange Rate between an mToken and the
 	/// underlying asset.
 	/// - [`borrow_rate`](`PoolState::borrow_rate`): Borrow Interest Rate
-	/// - [`supply_rate`](`PoolState::supply_rate`): current Supply Interest Rate.
+	/// - [`supply_rate`](`PoolState::supply_rate`): Supply Interest Rate.
 	///  The supply rate is derived from the borrow_rate and utilization_rate.
 	#[rpc(name = "controller_liquidityPoolState")]
 	fn liquidity_pool_state(&self, pool_id: CurrencyId, at: Option<BlockHash>) -> Result<Option<PoolState>>;
