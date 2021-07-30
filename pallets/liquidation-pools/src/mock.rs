4f00c19278f8dc2b891e9c8d85a0d9d3c499f15a
Eugene Yaroshenko <yaroshjeka@gmail.com>
2021-07-30 10:00:21 +0300

    Revert "[MIN-261] Remove test files"
    This reverts commit 6e67264d769fbd059856f1b752cb3f732d87a1d7.

---
+++ pallets/liquidation-pools/src/mock.rs
@@ -0,0 +1,280 @@
+/// Mocks for the liquidation-pools pallet.
+use super::*;
+use crate as liquidation_pools;
+use frame_support::{ord_parameter_types, parameter_types, PalletId};
+use frame_system::EnsureSignedBy;
+use minterest_primitives::Price;
+pub use minterest_primitives::{currency::CurrencyType::WrappedToken, Balance, CurrencyId, Rate};
+use orml_traits::parameter_type_with_key;
+use pallet_traits::PricesManager;
+use sp_core::H256;
+use sp_io::TestExternalities;
+use sp_runtime::testing::TestXt;
+use sp_runtime::{
+	testing::Header,
+	traits::{BlakeTwo256, IdentityLookup},
+	FixedPointNumber,
+};
+use sp_std::cell::RefCell;
+use std::collections::HashMap;
+pub use test_helper::*;
+
+type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
+type Block = frame_system::mocking::MockBlock<Test>;
+
+// Configure a mock runtime to test the pallet.
+frame_support::construct_runtime!(
+	pub enum Test where
+		Block = Block,
+		NodeBlock = Block,
+		UncheckedExtrinsic = UncheckedExtrinsic,
+	{
+		System: frame_system::{Pallet, Call, Config, Storage, Event<T>},
+		Balances: pallet_balances::{Pallet, Call, Storage, Config<T>, Event<T>},
+		//ORML palletts
+		Tokens: orml_tokens::{Pallet, Storage, Call, Event<T>, Config<T>},
+		Currencies: orml_currencies::{Pallet, Call, Event<T>},
+		// Minterest pallets
+		TestLiquidationPools: liquidation_pools::{Pallet, Storage, Call, Event<T>, ValidateUnsigned},
+		TestLiquidityPools: liquidity_pools::{Pallet, Storage, Call, Config<T>},
+		TestDex: dex::{Pallet, Storage, Call, Event<T>},
+		Controller: controller::{Pallet, Storage, Call, Event, Config<T>},
+		MntToken: mnt_token::{Pallet, Storage, Call, Event<T>, Config<T>},
+		MinterestModel: minterest_model::{Pallet, Storage, Call, Event, Config<T>},
+	}
+);
+
+parameter_types! {
+	pub const MntTokenPalletId: PalletId = PalletId(*b"min/mntt");
+	pub MntTokenAccountId: AccountId = MntTokenPalletId::get().into_account();
+}
+
+mock_impl_system_config!(Test);
+mock_impl_liquidity_pools_config!(Test);
+mock_impl_orml_tokens_config!(Test);
+mock_impl_orml_currencies_config!(Test);
+mock_impl_dex_config!(Test);
+mock_impl_balances_config!(Test);
+mock_impl_controller_config!(Test, ZeroAdmin);
+mock_impl_mnt_token_config!(Test, ZeroAdmin);
+mock_impl_minterest_model_config!(Test, ZeroAdmin);
+
+parameter_types! {
+	pub const LiquidityPoolsPalletId: PalletId = PalletId(*b"lqdy/min");
+	pub LiquidityPoolAccountId: AccountId = LiquidityPoolsPalletId::get().into_account();
+	pub InitialExchangeRate: Rate = Rate::one();
+	pub EnabledUnderlyingAssetsIds: Vec<CurrencyId> = CurrencyId::get_enabled_tokens_in_protocol(UnderlyingAsset);
+	pub EnabledWrappedTokensId: Vec<CurrencyId> = CurrencyId::get_enabled_tokens_in_protocol(WrappedToken);
+}
+
+thread_local! {
+	static UNDERLYING_PRICE: RefCell<HashMap<CurrencyId, Price>> = RefCell::new(
+		[
+			(DOT, Price::one()),
+			(ETH, Price::one()),
+			(BTC, Price::one()),
+			(KSM, Price::one()),
+		]
+		.iter()
+		.cloned()
+		.collect());
+}
+
+pub struct MockPriceSource;
+impl MockPriceSource {
+	pub fn set_underlying_price(currency_id: CurrencyId, price: Price) {
+		UNDERLYING_PRICE.with(|v| v.borrow_mut().insert(currency_id, price));
+	}
+}
+
+impl PricesManager<CurrencyId> for MockPriceSource {
+	fn get_underlying_price(currency_id: CurrencyId) -> Option<Price> {
+		UNDERLYING_PRICE.with(|v| v.borrow().get(&currency_id).copied())
+	}
+
+	fn lock_price(_currency_id: CurrencyId) {}
+
+	fn unlock_price(_currency_id: CurrencyId) {}
+}
+
+parameter_types! {
+	pub const LiquidationPoolsPalletId: PalletId = PalletId(*b"lqdn/min");
+	pub LiquidationPoolAccountId: AccountId = LiquidationPoolsPalletId::get().into_account();
+	pub const LiquidityPoolsPriority: TransactionPriority = TransactionPriority::max_value();
+}
+
+ord_parameter_types! {
+	pub const ZeroAdmin: AccountId = 0;
+}
+
+impl Config for Test {
+	type Event = Event;
+	type MultiCurrency = orml_tokens::Pallet<Test>;
+	type UnsignedPriority = LiquidityPoolsPriority;
+	type PriceSource = MockPriceSource;
+	type LiquidationPoolsPalletId = LiquidationPoolsPalletId;
+	type LiquidationPoolAccountId = LiquidationPoolAccountId;
+	type UpdateOrigin = EnsureSignedBy<ZeroAdmin, AccountId>;
+	type LiquidityPoolsManager = liquidity_pools::Pallet<Test>;
+	type Dex = dex::Pallet<Test>;
+	type LiquidationPoolsWeightInfo = ();
+	type ControllerManager = Controller;
+}
+
+/// An extrinsic type used for tests.
+pub type Extrinsic = TestXt<Call, ()>;
+
+impl<LocalCall> SendTransactionTypes<LocalCall> for Test
+where
+	Call: From<LocalCall>,
+{
+	type OverarchingCall = Call;
+	type Extrinsic = Extrinsic;
+}
+
+pub fn admin() -> Origin {
+	Origin::signed(ADMIN)
+}
+
+pub struct ExternalityBuilder {
+	endowed_accounts: Vec<(AccountId, CurrencyId, Balance)>,
+	liquidity_pools: Vec<(CurrencyId, PoolData)>,
+	liquidation_pools: Vec<(CurrencyId, LiquidationPoolData)>,
+}
+
+impl Default for ExternalityBuilder {
+	fn default() -> Self {
+		Self {
+			endowed_accounts: vec![],
+			liquidity_pools: vec![
+				(
+					DOT,
+					PoolData {
+						borrowed: Balance::zero(),
+						borrow_index: Rate::one(),
+						protocol_interest: Balance::zero(),
+					},
+				),
+				(
+					ETH,
+					PoolData {
+						borrowed: Balance::zero(),
+						borrow_index: Rate::one(),
+						protocol_interest: Balance::zero(),
+					},
+				),
+				(
+					BTC,
+					PoolData {
+						borrowed: Balance::zero(),
+						borrow_index: Rate::one(),
+						protocol_interest: Balance::zero(),
+					},
+				),
+			],
+			liquidation_pools: vec![
+				(
+					DOT,
+					LiquidationPoolData {
+						deviation_threshold: Rate::saturating_from_rational(1, 10),
+						balance_ratio: Rate::saturating_from_rational(2, 10),
+						max_ideal_balance_usd: None,
+					},
+				),
+				(
+					ETH,
+					LiquidationPoolData {
+						deviation_threshold: Rate::saturating_from_rational(1, 10),
+						balance_ratio: Rate::saturating_from_rational(2, 10),
+						max_ideal_balance_usd: None,
+					},
+				),
+				(
+					BTC,
+					LiquidationPoolData {
+						deviation_threshold: Rate::saturating_from_rational(1, 10),
+						balance_ratio: Rate::saturating_from_rational(2, 10),
+						max_ideal_balance_usd: None,
+					},
+				),
+				(
+					KSM,
+					LiquidationPoolData {
+						deviation_threshold: Rate::saturating_from_rational(1, 10),
+						balance_ratio: Rate::saturating_from_rational(2, 10),
+						max_ideal_balance_usd: None,
+					},
+				),
+			],
+		}
+	}
+}
+
+impl ExternalityBuilder {
+	pub fn user_balance(mut self, user: AccountId, currency_id: CurrencyId, balance: Balance) -> Self {
+		self.endowed_accounts.push((user, currency_id, balance));
+		self
+	}
+
+	pub fn set_pool_borrow_underlying(mut self, currency_id: CurrencyId, balance: Balance) -> Self {
+		self.liquidity_pools.push((
+			currency_id,
+			PoolData {
+				borrowed: balance,
+				borrow_index: Rate::one(),
+				protocol_interest: Balance::zero(),
+			},
+		));
+		self
+	}
+
+	pub fn liquidation_pool_balance(mut self, currency_id: CurrencyId, balance: Balance) -> Self {
+		self.endowed_accounts
+			.push((TestLiquidationPools::pools_account_id(), currency_id, balance));
+		self
+	}
+
+	pub fn dex_balance(mut self, currency_id: CurrencyId, balance: Balance) -> Self {
+		self.endowed_accounts
+			.push((TestDex::dex_account_id(), currency_id, balance));
+		self
+	}
+
+	pub fn build(self) -> TestExternalities {
+		let mut t = frame_system::GenesisConfig::default().build_storage::<Test>().unwrap();
+
+		orml_tokens::GenesisConfig::<Test> {
+			balances: self.endowed_accounts,
+		}
+		.assimilate_storage(&mut t)
+		.unwrap();
+
+		liquidity_pools::GenesisConfig::<Test> {
+			pools: self.liquidity_pools,
+			pool_user_data: vec![],
+		}
+		.assimilate_storage(&mut t)
+		.unwrap();
+
+		liquidation_pools::GenesisConfig::<Test> {
+			liquidation_pools: self.liquidation_pools,
+			phantom: PhantomData,
+		}
+		.assimilate_storage(&mut t)
+		.unwrap();
+
+		let mut ext = TestExternalities::new(t);
+		ext.execute_with(|| System::set_block_number(1));
+		ext
+	}
+}
+
+pub(crate) fn set_prices_for_assets(prices: Vec<(CurrencyId, Price)>) {
+	prices.into_iter().for_each(|(currency_id, price)| {
+		MockPriceSource::set_underlying_price(currency_id, price);
+	});
+}
+
+pub(crate) fn liquidation_pool_balance(pool_id: CurrencyId) -> Balance {
+	Currencies::free_balance(pool_id, &TestLiquidationPools::pools_account_id())
+}
---
+++ pallets/liquidation-pools/src/tests.rs
@@ -0,0 +1,539 @@
+//! Tests for the liquidation-pools pallet.
+
+use super::*;
+use frame_support::{assert_noop, assert_ok};
+use mock::{Event, *};
+use sp_core::offchain::{
+	testing::{TestOffchainExt, TestTransactionPoolExt},
+	OffchainDbExt, OffchainWorkerExt, TransactionPoolExt,
+};
+use sp_runtime::traits::{BadOrigin, Zero};
+
+use minterest_primitives::Price;
+
+#[test]
+fn offchain_worker_balancing_test() {
+	// balance ratio = 0.2 for two pools. Price the same.
+	// The offchain worker must send transaction for balancing.
+	// It must change 10_000 ETH to 10_000 DOT
+	let mut ext = ExternalityBuilder::default()
+		.liquidation_pool_balance(DOT, 10_000 * DOLLARS)
+		.liquidation_pool_balance(ETH, 30_000 * DOLLARS)
+		.set_pool_borrow_underlying(DOT, 100_000 * DOLLARS)
+		.set_pool_borrow_underlying(ETH, 100_000 * DOLLARS)
+		.build();
+	let (offchain, _) = TestOffchainExt::new();
+
+	let (pool, trans_pool_state) = TestTransactionPoolExt::new();
+	ext.register_extension(OffchainDbExt::new(offchain.clone()));
+	ext.register_extension(OffchainWorkerExt::new(offchain));
+	ext.register_extension(TransactionPoolExt::new(pool));
+
+	ext.execute_with(|| {
+		assert_ok!(TestLiquidationPools::_offchain_worker(0));
+
+		// 1 balancing transcation in transactions pool
+		assert_eq!(trans_pool_state.read().transactions.len(), 1);
+		let transaction = trans_pool_state.write().transactions.pop().unwrap();
+		let ex: Extrinsic = Decode::decode(&mut &*transaction).unwrap();
+		// Called extrinsic input params
+		let (supply_pool_id, target_pool_id, max_supply_amount, target_supply_amount) = match ex.call {
+			crate::mock::Call::TestLiquidationPools(crate::Call::balance_liquidation_pools(
+				supply_pool_id,
+				target_pool_id,
+				max_supply_amount,
+				target_supply_amount,
+			)) => (supply_pool_id, target_pool_id, max_supply_amount, target_supply_amount),
+			e => panic!("Unexpected call: {:?}", e),
+		};
+		assert_eq!(supply_pool_id, ETH);
+		assert_eq!(target_pool_id, DOT);
+		assert_eq!(max_supply_amount, 10_000 * DOLLARS);
+		assert_eq!(target_supply_amount, 10_000 * DOLLARS);
+	});
+}
+
+#[test]
+fn protocol_operations_not_working_for_nonexisting_pool() {
+	ExternalityBuilder::default().build().execute_with(|| {
+		assert_noop!(
+			TestLiquidationPools::set_deviation_threshold(admin(), KSM, 123),
+			Error::<Test>::PoolNotFound
+		);
+
+		assert_noop!(
+			TestLiquidationPools::set_balance_ratio(admin(), KSM, 123),
+			Error::<Test>::PoolNotFound
+		);
+
+		assert_noop!(
+			TestLiquidationPools::set_max_ideal_balance(admin(), KSM, Some(123)),
+			Error::<Test>::PoolNotFound
+		);
+
+		assert_noop!(
+			TestLiquidationPools::balance_liquidation_pools(Origin::none(), KSM, DOT, Balance::zero(), Balance::zero()),
+			Error::<Test>::PoolNotFound
+		);
+
+		assert_noop!(
+			TestLiquidationPools::transfer_to_liquidation_pool(admin(), KSM, 123),
+			Error::<Test>::PoolNotFound
+		);
+	});
+}
+
+#[test]
+fn set_deviation_threshold_should_work() {
+	ExternalityBuilder::default().build().execute_with(|| {
+		// Can be set to 0.0
+		assert_ok!(TestLiquidationPools::set_deviation_threshold(admin(), DOT, 0));
+		assert_eq!(
+			TestLiquidationPools::liquidation_pool_data_storage(DOT).deviation_threshold,
+			Rate::zero()
+		);
+		let expected_event = Event::TestLiquidationPools(crate::Event::DeviationThresholdChanged(DOT, Rate::zero()));
+		assert!(System::events().iter().any(|record| record.event == expected_event));
+
+		// Can be set to 1.0
+		assert_ok!(TestLiquidationPools::set_deviation_threshold(
+			admin(),
+			DOT,
+			1_000_000_000_000_000_000u128
+		));
+		assert_eq!(
+			TestLiquidationPools::liquidation_pool_data_storage(DOT).deviation_threshold,
+			Rate::one()
+		);
+		let expected_event = Event::TestLiquidationPools(crate::Event::DeviationThresholdChanged(DOT, Rate::one()));
+		assert!(System::events().iter().any(|record| record.event == expected_event));
+
+		// Can not be set grater than 1.0
+		assert_noop!(
+			TestLiquidationPools::set_deviation_threshold(admin(), DOT, 2_000_000_000_000_000_000u128),
+			Error::<Test>::NotValidDeviationThresholdValue
+		);
+
+		// The dispatch origin of this call must be Root or half MinterestCouncil.
+		assert_noop!(
+			TestLiquidationPools::set_deviation_threshold(alice_origin(), DOT, 10),
+			BadOrigin
+		);
+
+		// MDOT is wrong CurrencyId for underlying assets.
+		assert_noop!(
+			TestLiquidationPools::set_deviation_threshold(admin(), MDOT, 10),
+			Error::<Test>::NotValidUnderlyingAssetId
+		);
+	});
+}
+
+#[test]
+fn set_balance_ratio_should_work() {
+	ExternalityBuilder::default().build().execute_with(|| {
+		// Can be set to 0.0
+		assert_ok!(TestLiquidationPools::set_balance_ratio(admin(), DOT, 0));
+		assert_eq!(
+			TestLiquidationPools::liquidation_pool_data_storage(DOT).balance_ratio,
+			Rate::zero()
+		);
+		let expected_event = Event::TestLiquidationPools(crate::Event::BalanceRatioChanged(DOT, Rate::zero()));
+		assert!(System::events().iter().any(|record| record.event == expected_event));
+
+		// Can be set to 1.0
+		assert_ok!(TestLiquidationPools::set_balance_ratio(
+			admin(),
+			DOT,
+			1_000_000_000_000_000_000u128
+		));
+		assert_eq!(
+			TestLiquidationPools::liquidation_pool_data_storage(DOT).balance_ratio,
+			Rate::one()
+		);
+		let expected_event = Event::TestLiquidationPools(crate::Event::BalanceRatioChanged(DOT, Rate::one()));
+		assert!(System::events().iter().any(|record| record.event == expected_event));
+
+		// Can not be set grater than 1.0
+		assert_noop!(
+			TestLiquidationPools::set_balance_ratio(admin(), DOT, 2_000_000_000_000_000_000u128),
+			Error::<Test>::NotValidBalanceRatioValue
+		);
+
+		// The dispatch origin of this call must be Root or half MinterestCouncil.
+		assert_noop!(
+			TestLiquidationPools::set_balance_ratio(alice_origin(), DOT, 10),
+			BadOrigin
+		);
+
+		// MDOT is wrong CurrencyId for underlying assets.
+		assert_noop!(
+			TestLiquidationPools::set_balance_ratio(admin(), MDOT, 10),
+			Error::<Test>::NotValidUnderlyingAssetId
+		);
+	});
+}
+
+#[test]
+fn set_max_ideal_balance_should_work() {
+	ExternalityBuilder::default().build().execute_with(|| {
+		// Can be set to 0
+		assert_ok!(TestLiquidationPools::set_max_ideal_balance(
+			admin(),
+			DOT,
+			Some(Balance::zero())
+		));
+		assert_eq!(
+			TestLiquidationPools::liquidation_pool_data_storage(DOT).max_ideal_balance_usd,
+			Some(Balance::zero())
+		);
+		let expected_event =
+			Event::TestLiquidationPools(crate::Event::MaxIdealBalanceChanged(DOT, Some(Balance::zero())));
+		assert!(System::events().iter().any(|record| record.event == expected_event));
+
+		// Can be set to None
+		assert_ok!(TestLiquidationPools::set_max_ideal_balance(admin(), DOT, None));
+		assert_eq!(
+			TestLiquidationPools::liquidation_pool_data_storage(DOT).max_ideal_balance_usd,
+			None
+		);
+		let expected_event = Event::TestLiquidationPools(crate::Event::MaxIdealBalanceChanged(DOT, None));
+		assert!(System::events().iter().any(|record| record.event == expected_event));
+
+		// The dispatch origin of this call must be Root or half MinterestCouncil.
+		assert_noop!(
+			TestLiquidationPools::set_max_ideal_balance(alice_origin(), DOT, Some(10u128)),
+			BadOrigin
+		);
+
+		// MDOT is wrong CurrencyId for underlying assets.
+		assert_noop!(
+			TestLiquidationPools::set_max_ideal_balance(admin(), MDOT, Some(10u128)),
+			Error::<Test>::NotValidUnderlyingAssetId
+		);
+	});
+}
+
+#[test]
+fn calculate_pool_ideal_balance_usd_should_work() {
+	ExternalityBuilder::default()
+		.set_pool_borrow_underlying(DOT, 500_000 * DOLLARS)
+		.build()
+		.execute_with(|| {
+			// Check that ideal balance is calculated correctly when max_ideal_balance_usd is set to None
+			// Liquidity pool value: 500_000
+			// Oracle price: 1.0
+			// Balance ratio: 0.2
+			// Expected ideal balance: 100_000
+			assert_eq!(
+				TestLiquidationPools::calculate_pool_ideal_balance_usd(DOT),
+				Ok(100_000 * DOLLARS)
+			);
+
+			assert_ok!(TestLiquidationPools::set_max_ideal_balance(
+				admin(),
+				DOT,
+				Some(1_000 * DOLLARS)
+			));
+			// Check that ideal balance is calculated correctly when max_ideal_balance_usd is set to 1_000
+			// Liquidity pool value: 500_000
+			// Oracle price: 1.0
+			// Balance ratio: 0.2
+			// Expected ideal balance: min(100_000, 1_000) = 1_000
+			assert_eq!(
+				TestLiquidationPools::calculate_pool_ideal_balance_usd(DOT),
+				Ok(1_000 * DOLLARS)
+			);
+
+			assert_ok!(TestLiquidationPools::set_max_ideal_balance(
+				admin(),
+				DOT,
+				Some(1_000_000 * DOLLARS)
+			));
+			// Check that ideal balance is calculated correctly when max_ideal_balance_usd is set to 1_000_000
+			// Liquidity pool value: 500_000
+			// Oracle price: 1.0
+			// Balance ratio: 0.2
+			// Expected ideal balance: min(100_000, 1_000_000) = 100_000
+			assert_eq!(
+				TestLiquidationPools::calculate_pool_ideal_balance_usd(DOT),
+				Ok(100_000 * DOLLARS)
+			);
+		});
+}
+#[test]
+fn transfer_to_liquidation_pool_should_work() {
+	ExternalityBuilder::default()
+		.liquidation_pool_balance(DOT, 500_000)
+		.user_balance(ADMIN, DOT, 20_000)
+		.build()
+		.execute_with(|| {
+			let who = ensure_signed(admin());
+			// Check that transfer to liquidation pool works correctly
+			// Liquidation pool value: 500_000
+			// Transfer amount: 20_000
+			assert_ok!(TestLiquidationPools::transfer_to_liquidation_pool(admin(), DOT, 20_000));
+
+			let expected_event =
+				Event::TestLiquidationPools(crate::Event::TransferToLiquidationPool(DOT, 20_000, who.unwrap()));
+			assert!(System::events().iter().any(|record| record.event == expected_event));
+
+			assert_eq!(TestLiquidationPools::get_pool_available_liquidity(DOT), 520_000);
+
+			// Check that transfer with zero amount returns error.
+			// Transfer amount: 0
+			// Expected error: ZeroBalanceTransaction
+			assert_noop!(
+				TestLiquidationPools::transfer_to_liquidation_pool(admin(), DOT, 0),
+				Error::<Test>::ZeroBalanceTransaction
+			);
+
+			// Check thet transaction with unsuppurted asset returns error.
+			// Asset: MNT - native asset, underline assets are only allowed
+			// Expected error: NotValidUnderlyingAssetId
+			assert_noop!(
+				TestLiquidationPools::transfer_to_liquidation_pool(admin(), MNT, 20_000),
+				Error::<Test>::NotValidUnderlyingAssetId
+			);
+
+			// Check that attempt to transfer amount bigger that user balance returns error
+			// Transfer amount: 40_0000
+			// Balance: 0
+			assert_noop!(
+				TestLiquidationPools::transfer_to_liquidation_pool(admin(), DOT, 40_000),
+				Error::<Test>::NotEnoughLiquidityAvailable
+			);
+		});
+}
+
+// Description of the test:
+// Two liquidation pools have oversupply and two liquidation pools have shortfall.
+// Two "sales" are required for balancing.
+#[test]
+fn collects_sales_list_should_work_2_2() {
+	ExternalityBuilder::default()
+		.set_pool_borrow_underlying(DOT, 2_700_000 * DOLLARS)
+		.set_pool_borrow_underlying(KSM, 1_000_000 * DOLLARS)
+		.set_pool_borrow_underlying(ETH, 2_500_000_000 * DOLLARS)
+		.set_pool_borrow_underlying(BTC, 1_200_000 * DOLLARS)
+		.liquidation_pool_balance(DOT, 400_000 * DOLLARS)
+		.liquidation_pool_balance(KSM, 300_000 * DOLLARS)
+		.liquidation_pool_balance(ETH, 800_000_000 * DOLLARS)
+		.liquidation_pool_balance(BTC, 100_000 * DOLLARS)
+		.build()
+		.execute_with(|| {
+			set_prices_for_assets(vec![
+				(DOT, Price::saturating_from_integer(30)),
+				(KSM, Price::saturating_from_integer(5)),
+				(ETH, Price::saturating_from_integer(1_500)),
+				(BTC, Price::saturating_from_integer(50_000)),
+			]);
+
+			/*
+			Liquidity Pools balances (in assets): [2_700_000, 1_000_000, 2_500_000_000, 1_200_000]
+			Liquidity Pools balances (in USD): [81_000_000, 5_000_000, 3_750_000_000_000, 60_000_000_000]
+			Ideal balances 0.2 * liquidity_pool_balance (in USD): [16_200_000, 1_000_000,
+			750_000_000_000, 12_000_000_000]
+
+			Liquidation Pools balances (in assets): [400_000, 300_000, 800_000_000, 100_000]
+			Liquidation Pools balances (in USD): [12_000_000, 1_500_000, 1_200_000_000_000,
+			5_000_000_000]
+			Sales list (in assets): [(ETH, BTC, 140_000), (ETH, DOT, 140_000)]
+			*/
+			let expected_sales_list = vec![
+				Sales {
+					supply_pool_id: ETH,
+					target_pool_id: BTC,
+					amount_usd: 7_000_000_000 * DOLLARS, // USD equivalent
+				},
+				Sales {
+					supply_pool_id: ETH,
+					target_pool_id: DOT,
+					amount_usd: 4_200_000 * DOLLARS, // USD equivalent
+				},
+			];
+
+			assert_eq!(TestLiquidationPools::collects_sales_list(), Ok(expected_sales_list));
+		});
+}
+
+#[test]
+fn balance_liquidation_pools_should_work() {
+	ExternalityBuilder::default()
+		.set_pool_borrow_underlying(DOT, 500_000 * DOLLARS)
+		.set_pool_borrow_underlying(KSM, 1_000_000 * DOLLARS)
+		.set_pool_borrow_underlying(ETH, 1_500_000 * DOLLARS)
+		.set_pool_borrow_underlying(BTC, 2_000_000 * DOLLARS)
+		.liquidation_pool_balance(DOT, 400_000 * DOLLARS)
+		.liquidation_pool_balance(KSM, 300_000 * DOLLARS)
+		.liquidation_pool_balance(ETH, 200_000 * DOLLARS)
+		.liquidation_pool_balance(BTC, 100_000 * DOLLARS)
+		.dex_balance(DOT, 500_000 * DOLLARS)
+		.dex_balance(KSM, 500_000 * DOLLARS)
+		.dex_balance(ETH, 500_000 * DOLLARS)
+		.dex_balance(BTC, 500_000 * DOLLARS)
+		.build()
+		.execute_with(|| {
+			set_prices_for_assets(vec![
+				(DOT, Price::saturating_from_integer(1)),
+				(KSM, Price::saturating_from_integer(2)),
+				(ETH, Price::saturating_from_integer(5)),
+				(BTC, Price::saturating_from_integer(10)),
+			]);
+
+			/*
+			Liquidity Pools balances (in assets): [500_000, 1_000_000, 1_500_000, 2_000_000]
+			Liquidity Pools balances (in USD): [500_000, 2_000_000, 7_500_000, 20_000_000]
+			Ideal balances 0.2 * liquidity_pool_balance (in USD): [100_000, 400_000, 1_500_000, 4_000_000]
+
+			Liquidation Pools balances (in assets): [400_000, 300_000, 200_000, 100_000]
+			Liquidation Pools balances (in USD): [400_000, 600_000, 1_000_000, 1_000_000]
+
+			Sales list (in assets): [(DOT, BTC, 300_000$), (KSM, BTC, 200_000$)]
+
+			*/
+			let expected_sales_list = vec![
+				Sales {
+					supply_pool_id: DOT,
+					target_pool_id: BTC,
+					amount_usd: 300_000 * DOLLARS, // USD equivalent
+				},
+				Sales {
+					supply_pool_id: KSM,
+					target_pool_id: BTC,
+					amount_usd: 200_000 * DOLLARS, // USD equivalent
+				},
+			];
+
+			assert_eq!(
+				TestLiquidationPools::collects_sales_list(),
+				Ok(expected_sales_list.clone())
+			);
+
+			expected_sales_list.iter().for_each(|sale| {
+				if let Some((max_supply_amount, target_amount)) =
+					TestLiquidationPools::get_amounts(sale.supply_pool_id, sale.target_pool_id, sale.amount_usd).ok()
+				{
+					let _ = TestLiquidationPools::balance_liquidation_pools(
+						Origin::none(),
+						sale.supply_pool_id,
+						sale.target_pool_id,
+						max_supply_amount,
+						target_amount,
+					);
+				};
+			});
+
+			// Test that the expected events were emitted
+			let our_events = System::events()
+				.into_iter()
+				.map(|r| r.event)
+				.filter_map(|e| {
+					if let Event::TestDex(inner) = e {
+						Some(inner)
+					} else {
+						None
+					}
+				})
+				.collect::<Vec<_>>();
+			let expected_events = vec![
+				dex::Event::Swap(
+					TestLiquidationPools::pools_account_id(),
+					DOT,
+					BTC,
+					300_000 * DOLLARS, // max_supply_amount = 300_000 DOT
+					30_000 * DOLLARS,  // target_amount = 30_000 BTC
+				),
+				dex::Event::Swap(
+					TestLiquidationPools::pools_account_id(),
+					KSM,
+					BTC,
+					100_000 * DOLLARS, // max_supply_amount = 100_000 DOT
+					20_000 * DOLLARS,  // target_amount = 20_000 BTC
+				),
+			];
+			assert_eq!(our_events, expected_events);
+
+			// Liquidation Pool balances
+			assert_eq!(liquidation_pool_balance(DOT), 100_000 * DOLLARS);
+			assert_eq!(liquidation_pool_balance(KSM), 200_000 * DOLLARS);
+			assert_eq!(liquidation_pool_balance(ETH), 200_000 * DOLLARS);
+			assert_eq!(liquidation_pool_balance(BTC), 150_000 * DOLLARS);
+		});
+}
+
+#[test]
+fn balance_liquidation_pools_two_pools_should_work_test() {
+	ExternalityBuilder::default()
+		.set_pool_borrow_underlying(DOT, 500_000 * DOLLARS)
+		.set_pool_borrow_underlying(ETH, 300_000 * DOLLARS)
+		.liquidation_pool_balance(DOT, 170_000 * DOLLARS) // + 140_000$
+		.liquidation_pool_balance(ETH, 30_000 * DOLLARS) //- 120_000$
+		.dex_balance(DOT, 500_000 * DOLLARS)
+		.dex_balance(ETH, 500_000 * DOLLARS)
+		.build()
+		.execute_with(|| {
+			set_prices_for_assets(vec![
+				(DOT, Price::saturating_from_integer(2)),
+				(ETH, Price::saturating_from_integer(4)),
+			]);
+
+			/*
+			Liquidity Pools balances (in assets): [500_000, 300_000]
+			Liquidity Pools balances (in USD): [1_000_000, 1_200_000]
+			Liquidation Pools balances (in assets): [170_000, 30_000]
+			Liquidation Pools balances (in USD):                  [340_000 (+140_000$), 120_000 (-120_000$)]
+			Ideal balances 0.2 * liquidity_pool_balance (in USD): [200_000, 240_000]
+			Sales list (in assets): [(DOT, ETH, 120_000$)
+			*/
+			let expected_sales_list = vec![Sales {
+				supply_pool_id: DOT,
+				target_pool_id: ETH,
+				amount_usd: 120_000 * DOLLARS, // USD equivalent
+			}];
+
+			assert_eq!(
+				TestLiquidationPools::collects_sales_list(),
+				Ok(expected_sales_list.clone())
+			);
+
+			expected_sales_list.iter().for_each(|sale| {
+				if let Some((max_supply_amount, target_amount)) =
+					TestLiquidationPools::get_amounts(sale.supply_pool_id, sale.target_pool_id, sale.amount_usd).ok()
+				{
+					let _ = TestLiquidationPools::balance_liquidation_pools(
+						Origin::none(),
+						sale.supply_pool_id,
+						sale.target_pool_id,
+						max_supply_amount,
+						target_amount,
+					);
+				};
+			});
+
+			// Test that the expected events were emitted
+			let our_events = System::events()
+				.into_iter()
+				.map(|r| r.event)
+				.filter_map(|e| {
+					if let Event::TestDex(inner) = e {
+						Some(inner)
+					} else {
+						None
+					}
+				})
+				.collect::<Vec<_>>();
+			let expected_events = vec![dex::Event::Swap(
+				TestLiquidationPools::pools_account_id(),
+				DOT,
+				ETH,
+				60_000 * DOLLARS, // max_supply_amount = 60_000 DOT
+				30_000 * DOLLARS, // target_amount = 30_000 ETH
+			)];
+
+			assert_eq!(our_events, expected_events);
+
+			// Liquidation Pool balances
+			assert_eq!(liquidation_pool_balance(DOT), 110_000 * DOLLARS);
+			assert_eq!(liquidation_pool_balance(ETH), 60_000 * DOLLARS);
+		});
+}
