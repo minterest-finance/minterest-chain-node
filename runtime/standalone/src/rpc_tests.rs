0180e97d849fb9d8ccac39643b2b100dabd4ef64
Eugene Yaroshenko <yaroshjeka@gmail.com>
2021-08-09 22:28:00 +0300

    [MIN-367] Fix test rpc

--- runtime/standalone/src/rpc_tests.rs
+++ runtime/standalone/src/rpc_tests.rs
@@ -1466,7 +1466,7 @@ fn get_all_locked_prices_rpc_should_work() {
 				DOT => {
 					assert_eq!(price, None);
 				}
-				ETH | BTC | KSM => {
+				ETH | BTC | KSM | MNT => {
 					assert_eq!(price, Some(Price::saturating_from_integer(10_000)));
 				}
 				_ => panic!("Unexpected token!"),
